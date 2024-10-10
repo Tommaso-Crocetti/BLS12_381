c979ec924f04d0fcfbc909a38e5e840939fa733e
Tommaso-Crocetti <t.crocetti1@studenti.unipi.it>
2024-10-10 22:25:03 +0200

    Test environment update - Fields

---
+++ contracts/curve.sol
@@ -0,0 +1,91 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./field/BigFiniteField.sol";
+import "./field/quadraticExtension.sol";
+import "./field/sexticExtension.sol";
+import "./field/twelveExtension.sol";
+
+contract Curve {
+    enum PointType {
+        Affine,
+        PointAtInfinity
+    }
+
+    struct Point_0 {
+        PointType pointType;
+        Zp x;
+        Zp y;
+    }
+
+    struct Point_1 {
+        PointType pointType;
+        Zp_2 x;
+        Zp_2 y;
+    }
+
+    struct Point_2 {
+        PointType pointType;
+        Zp_12 x;
+        Zp_12 y;
+    }
+
+    BigNumber private order;
+    Point_0 private g0;
+    Point_1 private g1;
+    FiniteField private fField;
+    QuadraticExtension private qField;
+    SexticExtension private sField;
+    TwelveExtension private tField;
+
+    function isOnCurve_0(
+        FiniteField f,
+        Point_0 memory p
+    ) public view returns (bool) {
+        if (p.pointType == PointType.PointAtInfinity) return false;
+        Zp memory l = f.mul(p.y, p.y);
+        Zp memory r = f.sum(
+            f.mul(f.mul(p.x, p.x), p.x),
+            f.mul_nonres(f.createElement(4))
+        );
+        return f.equals(l, r);
+    }
+
+    function isOnCurve_1(
+        QuadraticExtension q,
+        Point_1 memory p
+    ) public view returns (bool) {
+        if (p.pointType == PointType.PointAtInfinity) return false;
+        Zp_2 memory l = q.mul(p.y, p.y);
+        Zp_2 memory r = q.sum(
+            q.mul(q.mul(p.x, p.x), p.x),
+            q.mul_nonres(q.createElement(Zp(4), fField.zero()))
+        );
+        return q.equals(l, r);
+    }
+
+    function untwist(Point_1 memory p) public view returns (Point_2 memory) {
+        Zp_6 memory t0 = sField.createElement(
+            qField.zero(),
+            qField.createElement(fField.createElement(1), fField.zero()),
+            qField.zero()
+        );
+        Zp_12 memory t1 = tField.createElement(
+            sField.createElement(p.x, qField.zero(), qField.zero()),
+            sField.zero()
+        );
+        Zp_12 memory t2 = tField.inverse(
+            tField.createElement(sField.zero(), t0)
+        );
+        Zp_12 memory t3 = tField.createElement(
+            sField.createElement(p.y, qField.zero(), qField.zero()),
+            sField.zero()
+        );
+        Zp_12 memory t4 = tField.inverse(
+            tField.createElement(t0, sField.zero())
+        );
+
+        return
+            Point_2(PointType.Affine, tField.mul(t1, t2), tField.mul(t3, t4));
+    }
+}
--- test/SexticExtension.ts
+++ test/SexticExtension.ts
@@ -1,20 +1,23 @@
 import { ethers } from "hardhat";
 import { expect } from "chai";
-import { FiniteField, FiniteField__factory } from "../typechain-types"; 
+import { BigFiniteField, BigFiniteField__factory, BigNumbers, BigNumbers__factory } from "../typechain-types"; // Assicurati che il percorso sia corretto
 import { QuadraticExtension, QuadraticExtension__factory } from "../typechain-types/"
 import { SexticExtension, SexticExtension__factory } from "../typechain-types";
-import { ZpStruct, ZpStructOutput } from "../typechain-types/field/finiteField.sol/FiniteField";
+import { BigNumberStruct, BigNumberStructOutput } from "../typechain-types/BigNumber.sol/BigNumbers";
+import { ZpStruct, ZpStructOutput } from "../typechain-types/field/BigFiniteField";
 import { Zp_2Struct, Zp_2StructOutput } from "../typechain-types/field/quadraticExtension.sol/QuadraticExtension";
 import { Zp_6Struct, Zp_6StructOutput } from "../typechain-types/field/sexticExtension.sol/SexticExtension";
 
+function toBigNumber(input: BigNumberStructOutput): BigNumberStruct {
+    return {val: input.val, neg: input.neg, bitlen: input.bitlen };
+}
 
-// Utility functions to convert structs
 function toZpStruct(output: ZpStructOutput): ZpStruct {
-  return { value: output.value };
+    return { value: toBigNumber(output.value) }; // Restituisce un oggetto con la proprietà value
 }
 
 function toZp_2Struct(output: Zp_2StructOutput): Zp_2Struct {
-  return { a: toZpStruct(output.a), b: toZpStruct(output.b) };
+    return { a: toZpStruct(output.a), b: toZpStruct(output.b) }; // Restituisce un oggetto con la proprietà value
 }
 
 function toZp_6Struct(output: Zp_6StructOutput): Zp_6Struct {
@@ -26,118 +29,92 @@ function toZp_6Struct(output: Zp_6StructOutput): Zp_6Struct {
 }
 
 describe("Sextic Extension Contract", function () {
-    let finiteField: FiniteField;
+    let bigNumbers: BigNumbers;
+    let bigFiniteField: BigFiniteField;
     let quadraticExtension: QuadraticExtension;
     let sexticExtension: SexticExtension;
-    let element6x: Zp_6StructOutput;
-    let element6y: Zp_6StructOutput;
+    let x: Zp_6StructOutput;
+    let y: Zp_6StructOutput;
 
     beforeEach(async function () {
-        const FiniteFieldFactory: FiniteField__factory = (await ethers.getContractFactory("FiniteField")) as FiniteField__factory;
-        finiteField = await FiniteFieldFactory.deploy(7);
-
-        const QuadraticExtensionFactory: QuadraticExtension__factory = (await ethers.getContractFactory("QuadraticExtension")) as QuadraticExtension__factory;
-        quadraticExtension = await QuadraticExtensionFactory.deploy(finiteField);
+        const bigNumbersFactory: BigNumbers__factory = await ethers.getContractFactory("BigNumbers") as BigNumbers__factory;
+        bigNumbers = await bigNumbersFactory.deploy();
+        const bigFiniteFieldFactory: BigFiniteField__factory = await ethers.getContractFactory("BigFiniteField", {
+            libraries: {
+                BigNumbers: await bigNumbers.getAddress()
+            }
+        }) as BigFiniteField__factory;
+        const value = 7;
+        bigFiniteField = await bigFiniteFieldFactory.deploy(ethers.toBeHex(value.toString()));
+        const quadraticExtensionFactory: QuadraticExtension__factory = await ethers.getContractFactory("QuadraticExtension") as QuadraticExtension__factory;
+        quadraticExtension = await quadraticExtensionFactory.deploy(bigFiniteField);
 
         const SexticExtensionFactory: SexticExtension__factory = (await ethers.getContractFactory("SexticExtension")) as SexticExtension__factory;
         sexticExtension = await SexticExtensionFactory.deploy(quadraticExtension);
 
-        const a: ZpStructOutput = await finiteField.createElement(3);
-        const b: ZpStructOutput = await finiteField.createElement(5);
-        const x: Zp_2StructOutput = await quadraticExtension.createElement(toZpStruct(a), toZpStruct(b));
+        const a: ZpStructOutput = await bigFiniteField.createElement(toBigNumber(await bigNumbers.init(3, false)));
+        const b: ZpStructOutput = await bigFiniteField.createElement(toBigNumber(await bigNumbers.init(5, false)));
+        const x_2: Zp_2StructOutput = await quadraticExtension.createElement(toZpStruct(a), toZpStruct(b));
 
-        const c: ZpStructOutput = await finiteField.createElement(1);
-        const d: ZpStructOutput = await finiteField.createElement(2);
-        const y: Zp_2StructOutput = await quadraticExtension.createElement(toZpStruct(c), toZpStruct(d));
+        const c: ZpStructOutput = await bigFiniteField.createElement(toBigNumber(await bigNumbers.init(1, false)));
+        const d: ZpStructOutput = await bigFiniteField.createElement(toBigNumber(await bigNumbers.init(2, false)));
+        const y_2: Zp_2StructOutput = await quadraticExtension.createElement(toZpStruct(c), toZpStruct(d));
 
-        element6x = await sexticExtension.createElement(toZp_2Struct(x), toZp_2Struct(y), toZp_2Struct(x));
-        element6y = await sexticExtension.createElement(toZp_2Struct(y), toZp_2Struct(x), toZp_2Struct(y));
+        x = await sexticExtension.createElement(toZp_2Struct(x_2), toZp_2Struct(y_2), toZp_2Struct(x_2));
+        y = await sexticExtension.createElement(toZp_2Struct(y_2), toZp_2Struct(x_2), toZp_2Struct(y_2));
     });
 
     it("should create sextic field elements correctly", async function () {
-        const a: ZpStructOutput = await finiteField.createElement(3);
-        const b: ZpStructOutput = await finiteField.createElement(5);
-        const x: Zp_2StructOutput = await quadraticExtension.createElement(toZpStruct(a), toZpStruct(b));
-
-        const c: ZpStructOutput = await finiteField.createElement(1);
-        const d: ZpStructOutput = await finiteField.createElement(2);
-        const y: Zp_2StructOutput = await quadraticExtension.createElement(toZpStruct(c), toZpStruct(d));
-
-        const element6: Zp_6StructOutput = await sexticExtension.createElement(toZp_2Struct(x), toZp_2Struct(y), toZp_2Struct(x));
-        expect(element6.a.a.value).to.equal(3);
-        expect(element6.b.a.value).to.equal(1);
-        expect(element6.c.a.value).to.equal(3);
+        expect(x.a.a.value.val).to.equal("0x0000000000000000000000000000000000000000000000000000000000000003");
+        expect(x.b.a.value.val).to.equal("0x0000000000000000000000000000000000000000000000000000000000000001");
+        expect(x.c.a.value.val).to.equal("0x0000000000000000000000000000000000000000000000000000000000000003");
     });
 
     it("should add sextic elements correctly", async function () {
-        const sum: Zp_6StructOutput = await sexticExtension.sum(toZp_6Struct(element6x), toZp_6Struct(element6y));
-
-        expect(sum.a.a.value).to.equal(4);
-        expect(sum.b.a.value).to.equal(4);
-        expect(sum.c.a.value).to.equal(4);
-        expect(sum.a.b.value).to.equal(0);
-        expect(sum.b.b.value).to.equal(0);
-        expect(sum.c.b.value).to.equal(0);
+        const sum: Zp_6StructOutput = await sexticExtension.sum(toZp_6Struct(x), toZp_6Struct(y));
+        const resultA_A = await bigNumbers.init(4, false);
+        const resultB_A = await bigNumbers.init(4, false);
+        const resultC_A = await bigNumbers.init(4, false);
+        const resultA_B = await bigNumbers.zero();
+        const resultB_B = await bigNumbers.zero();
+        const resultC_B = await bigNumbers.zero();
+        expect(sum.a.a.value.val).to.equal(resultA_A.val);
+        expect(sum.b.a.value.val).to.equal(resultB_A.val);
+        expect(sum.c.a.value.val).to.equal(resultC_A.val);
+        expect(sum.a.b.value.val).to.equal(resultA_B.val);
+        expect(sum.b.b.value.val).to.equal(resultB_B.val);
+        expect(sum.c.b.value.val).to.equal(resultC_B.val);
     });
 
     it("should subtract sextic elements correctly", async function () {
-
-        const sub: Zp_6StructOutput = await sexticExtension.sub(toZp_6Struct(element6x), toZp_6Struct(element6y));
-
-        expect(sub.a.a.value).to.equal(2);
-        expect(sub.b.a.value).to.equal(5);
-        expect(sub.c.a.value).to.equal(2);
-        expect(sub.a.b.value).to.equal(3);
-        expect(sub.b.b.value).to.equal(4);
-        expect(sub.c.b.value).to.equal(3);
-
+        const sub: Zp_6StructOutput = await sexticExtension.sub(toZp_6Struct(x), toZp_6Struct(y));
+        const resultA_A = await bigNumbers.init(2, false);
+        const resultB_A = await bigNumbers.init(5, false);
+        const resultC_A = await bigNumbers.init(2, false);
+        const resultA_B = await bigNumbers.init(3, false);
+        const resultB_B = await bigNumbers.init(4, false);
+        const resultC_B = await bigNumbers.init(3, false);
+        expect(sub.a.a.value.val).to.equal(resultA_A.val);
+        expect(sub.b.a.value.val).to.equal(resultB_A.val);
+        expect(sub.c.a.value.val).to.equal(resultC_A.val);
+        expect(sub.a.b.value.val).to.equal(resultA_B.val);
+        expect(sub.b.b.value.val).to.equal(resultB_B.val);
+        expect(sub.c.b.value.val).to.equal(resultC_B.val);
     });
 
     it("should multiply elements in Zp_6 correctly", async function () {
-        // Creazione degli elementi Zp_2 per l'elemento x in Zp_6
-        const x_a: Zp_2StructOutput = await quadraticExtension.createElement(
-            { value: 3 }, { value: 5 }
-        );
-        const x_b: Zp_2StructOutput = await quadraticExtension.createElement(
-            { value: 1 }, { value: 2 }
-        );
-        const x_c: Zp_2StructOutput = await quadraticExtension.createElement(
-            { value: 4 }, { value: 0 }
-        );
-        const x = await sexticExtension.createElement(
-            toZp_2Struct(x_a), toZp_2Struct(x_b), toZp_2Struct(x_c)
-        );
-
-        // Creazione degli elementi Zp_2 per l'elemento y in Zp_6
-        const y_a: Zp_2StructOutput = await quadraticExtension.createElement(
-            { value: 2 }, { value: 6 }
-        );
-        const y_b: Zp_2StructOutput = await quadraticExtension.createElement(
-            { value: 1 }, { value: 3 }
-        );
-        const y_c: Zp_2StructOutput = await quadraticExtension.createElement(
-            { value: 0 }, { value: 1 }
-        );
-        const y = await sexticExtension.createElement(
-            toZp_2Struct(y_a), toZp_2Struct(y_b), toZp_2Struct(y_c)
-        );
-
-        // Moltiplicazione tra x e y
-        const result = await sexticExtension.mul(toZp_6Struct(x), toZp_6Struct(y));
-
-        // Risultati attesi
-        const expected_a = await quadraticExtension.createElement({ value: 5 }, { value: 3 });
-        const expected_b = await quadraticExtension.createElement({ value: 3 }, { value: 6 });
-        const expected_c = await quadraticExtension.createElement({ value: 5 }, { value: 4 });
-
-        // Assert: Verifica se il risultato della moltiplicazione è corretto
-        expect(result.a.a.value).to.equal(expected_a.a.value);
-        expect(result.a.b.value).to.equal(expected_a.b.value);
-
-        expect(result.b.a.value).to.equal(expected_b.a.value);
-        expect(result.b.b.value).to.equal(expected_b.b.value);
-
-        expect(result.c.a.value).to.equal(expected_c.a.value);
-        expect(result.c.b.value).to.equal(expected_c.b.value);
+        const mul = await sexticExtension.mul(toZp_6Struct(x), toZp_6Struct(y));
+        const resultA_A = await bigNumbers.init(1, false);
+        const resultB_A = await bigNumbers.init(6, false);
+        const resultC_A = await bigNumbers.init(0, false);
+        const resultA_B = await bigNumbers.init(0, false);
+        const resultB_B = await bigNumbers.init(2, false);
+        const resultC_B = await bigNumbers.init(5, false);
+        expect(mul.a.a.value.val).to.equal(resultA_A.val);
+        expect(mul.b.a.value.val).to.equal(resultB_A.val);
+        expect(mul.c.a.value.val).to.equal(resultC_A.val);
+        expect(mul.a.b.value.val).to.equal(resultA_B.val);
+        expect(mul.b.b.value.val).to.equal(resultB_B.val);
+        expect(mul.c.b.value.val).to.equal(resultC_B.val);
     });
 });
